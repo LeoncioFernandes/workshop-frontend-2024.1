0ebd2988ad4a8cfcb378941e5976c8ba0c60962b
Leoncio Fernandes de Oliveira Filho <leonciofernandes@gmail.com>
2024-03-10 15:29:08 -0300

    Correção de cores e implementação de Shadows

--- src/app/page.js
+++ src/app/page.js
@@ -20,20 +20,20 @@ export default function Home({pesquisa}) {
   };
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-12 bg-[#141414] ">
+    <main className="flex min-h-screen flex-col items-center justify-between p-12 bg-[#141414] ">
       <div className="flex flex-row flex-wrap justify-around gap-10">
         {data
           .filter((item) => item.name.toLowerCase().includes(pesquisa))
           .map((item) => (
           
             <div key={item.id} className="flex flex-col w-1/6 ">
-              <h2 className="py-3 bg-[#54b9c5] text-white font-bold text-xl rounded-t-xl text-center">{item.name}</h2>
-              <img src={item.image.medium} className="rounded-b-xl" />
+              <h2 className="py-3 bg-[#141414] shadow-2xl shadow-[#7a7a7a] text-white font-bold text-xl rounded-t-xl text-center">{item.name}</h2>
+              <img src={item.image.medium} className="rounded-b-xl shadow-[0_0px_40px_-15px_rgba(0,0,0,0.3)] shadow-[#7a7a7a]" />
               <div className="text-white text-justify text-base pt-2" dangerouslySetInnerHTML={createMarkup(item.summary)}></div>
             </div>
           ))
         }
       </div>
-    </div>
+    </main>
   );
 }
